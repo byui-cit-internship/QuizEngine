18ba9c1a011c63d5fb3841d688baa4971e9290f3
davisa94 <austin.i.davis94@gmail.com>
2020-09-14 13:11:25 -0600

    removed chaff, and added a setup script

---
+++ designDocs/setup.ps1
@@ -0,0 +1,14 @@
+npm install -g @vue/cli @vue/cli-service-global
+vue create quiz-engine
+cd quiz-engine
+vue add express
+mkdir backend
+cd backend
+npm init
+npm i body-parser cors express
+npm install nodemon
+
+
+
+
+
