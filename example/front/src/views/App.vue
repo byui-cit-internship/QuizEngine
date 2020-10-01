8aeb4af33846c8d6f00fec5151fdeb6a341f1ac6
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2020-10-01 12:59:29 -0600

    Tutorial (#11)
    * Added the tutorial for the BFF to use.
    
    * added .env file and a script to create a blank one
    
    * Fixed a path issue
    
    * really fixed?
    
    * Added a dot
    
    * added dotenv to the dependencies
    
    * Fixed dotenv  #1
    
    * just a short test
    
    * Removed a console log
    
    * Initialized the vue project for front end and added some files to the gitIgnore
    
    * Updated Code for several files according to the tutorial
    
    * Created baseURL in front
    
    * created Tutorial Data Service
    
    * Populated Add tutorial from tutorial
    
    * populated TutorialsList and added config
    
    * moved project
    
    * moved front end back to vue js 2 and it functions
    
    * Change routes for delete/delete all. Added nodemon to scripts (npm run serve)
    
    * Added some comments, melted my brain
    
    * Navbar to component
    
    * restructure
    
    Co-authored-by: davisa94 <austin.i.davis94@gmail.com>
    Co-authored-by: teelusk <teelusk@users.noreply.github.com>

---
+++ example/front/src/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+  },
+};
+</script>
