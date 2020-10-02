5f4937c636f6d611bfcf93e09f9f4402f8d4d2f9
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2020-10-02 17:07:18 -0600

    Add Vuetify, frontend scaffolding, navbar start

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+      <!-- <HelloWorld /> -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+import Navbar from "./components/NavBar";
+
+export default {
+  name: "App",
+
+  components: {
+    // HelloWorld,
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
