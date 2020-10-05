7ae95d75fcd802d3297090c97d8029107033b362
teelusk <teelusk@users.noreply.github.com>
2020-10-05 10:21:18 -0600

    Somewhat working NavBar

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="navbar">
-    <v-app-bar>
+    <v-app-bar app height="75px">
+      <v-btn icon class="ml-2">
+        <v-icon>mdi-book</v-icon>
+      </v-btn>
+
+      <v-toolbar-title>Quiz Engine</v-toolbar-title>
+      <v-spacer />
+
+      <v-btn class="mx-2">Account</v-btn>
+      <v-btn>
+        <v-icon> mdi-magnify </v-icon>
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
