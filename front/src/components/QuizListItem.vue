34f54a455887eda5ab8f5e8a365bacfdc125678a
davisa94 <austin.i.davis94@gmail.com>
2020-10-07 13:35:05 -0600

    changed quiz item name

--- front/src/components/QuizListItem.vue
+++ front/src/components/QuizListItem.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container>
         <v-card>
-            <v-card-title v-text="quiz.title"/>
-            <v-card-subtitle v-for="subject in quiz.subjects" :key="subject.index">
+            <v-card-title v-text="quizitem.title"/>
+            <v-card-subtitle v-for="subject in quizitem.subjects" :key="subject.index">
                 {{subject}}
             </v-card-subtitle>
         </v-card>
