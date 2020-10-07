024162ec20e7b4796ef97cf2a1af3812a3c2f587
davisa94 <austin.i.davis94@gmail.com>
2020-10-07 12:24:04 -0600

    Created quiz list item

---
+++ front/src/components/QuizListItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-title v-text="quiz.title"/>
+
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "QuizListItem",
+    data:()=>
+}
+</script>
