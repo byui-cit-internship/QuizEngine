6413d2d4df781568559313501fc6156585f3ceb7
teelusk <teelusk@users.noreply.github.com>
2020-10-07 13:03:55 -0600

    Quiz List

--- front/src/components/QuizList.vue
+++ front/src/components/QuizList.vue
@@ -1,60 +1,33 @@
 <template>
   <div class="quizList">
-    <!-- <v-card class="mx-auto mt-5" max-width="344">
-      <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-        height="200px"
-      ></v-img>
-
-      <v-card-title> Top western road trips </v-card-title>
-
-      <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn color="orange lighten-2" text> Explore </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon @click="show = !show">
-          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-
-          <v-card-text>
-            I'm a thing. But, like most politicians, he promised more than he
-            could deliver. You won't have time for sleeping, soldier, not with
-            all the bed making you'll be doing. Then we'll go with that data
-            file! Hey, you add a one and two zeros to that or we walk! You're
-            going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card> -->
-
     <v-card>
       <v-card-title>Select a Subject/Quiz</v-card-title>
 
-      <!-- TODO Show Unique Subjects in Select List -->
+      <!-- Show Unique Subjects in Select List -->
       <v-container fluid>
         <v-autocomplete
-          v-model="select"
+          v-model="selectedSubjects"
+          v-on:select="getFilteredQuizzes"
           label="Subjects"
           :items="getUniqueSubjects"
           chips
           deletable-chips
+          clearable
           multiple
         ></v-autocomplete>
-      </v-container>
 
-      <!-- TODO Show Filtered Quizzes based on Subject -->
-      <v-subheader>Quiz</v-subheader>
-      <v-list-item v-for="quiz in quizzes" :key="quiz.id">
-        {{ quiz.title }}
-      </v-list-item>
+        <!-- Displays Subject List Items based on Selected Subjects -->
+        <v-subheader>Selected Subjects</v-subheader>
+        <v-list-item v-for="subject in selectedSubjects" :key="subject.index">
+          {{ subject }}
+        </v-list-item>
+
+        <!-- TODO Show Filtered Quizzes based on Subject (Dynamic List Rendering) -->
+        <v-subheader>Quiz</v-subheader>
+        <v-list-item v-for="quiz in filteredQuizzes" :key="quiz.index">
+          {{ quiz }}
+        </v-list-item>
+      </v-container>
     </v-card>
   </div>
 </template>
@@ -64,31 +37,47 @@ export default {
   name: "QuizList",
   data: () => ({
     show: false,
+    selectedSubjects: [],
+    filteredQuizzes: [],
     quizzes: [
       { id: 1, title: "Quiz 1", subject: "Database" },
       { id: 2, title: "Quiz 2", subject: "Database" },
       { id: 3, title: "Quiz 3", subject: "Security" },
       { id: 4, title: "Quiz 4", subject: "Security" },
       { id: 5, title: "Quiz 5", subject: "Software Engineering" },
+      { id: 6, title: "Quiz 6", subject: "Software Engineering" },
+      { id: 7, title: "Quiz 7", subject: "Front End Engineering" },
+      { id: 8, title: "Quiz 8", subject: "Back End Engineering" },
     ],
-    items: ["foo", "bar", "fizz", "buzz", "fizzbuzz", "foobar"],
-    value: ["foo", "bar", "fizz"],
   }),
+  methods: {},
+
   computed: {
     getUniqueSubjects() {
       let array = this.quizzes.map((a) => a.subject);
       //Only unique values allowed in ES6 Set. Way of filtering out non-unique values
-      let uniqueSubjects = Array.from(new Set(array));
+      let uniqueSubjects = Array.from(new Set(array)).sort();
       return uniqueSubjects;
+    },
+
+    clearFilteredList() {
+      let filteredQuizzes = this.filteredQuizzes;
+      filteredQuizzes = [];
+      return filteredQuizzes;
     },
 
     getFilteredQuizzes() {
       // for each quiz in quizzes, if quiz.id is in any of selected subjects array, push quiz.title to selected quizzes array
       // TODO How to handle event of new subject being selected
+
+      this.clearFilteredList;
+      let selectedSubjects = this.selectedSubjects;
+      let filteredQuizzes = this.filteredQuizzes;
       this.quizzes.forEach((element) => {
-        return element.title;
+        if (element.subject.includes(selectedSubjects)) {
+          filteredQuizzes.push(element.title);
+        }
       });
-      let filteredQuizzes = [];
       return filteredQuizzes;
     },
   },
