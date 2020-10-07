afbc626131e74a7a7465b205d41bc2894c20e3f5
teelusk <teelusk@users.noreply.github.com>
2020-10-07 13:07:59 -0600

    It's working

--- front/src/components/QuizList.vue
+++ front/src/components/QuizList.vue
@@ -16,7 +16,6 @@
           multiple
         ></v-autocomplete>
 
-<<<<<<< HEAD
         <!-- Displays Subject List Items based on Selected Subjects -->
         <v-subheader>Selected Subjects</v-subheader>
         <v-list-item v-for="subject in selectedSubjects" :key="subject.index">
@@ -29,22 +28,20 @@
           {{ quiz }}
         </v-list-item>
       </v-container>
-=======
       <!-- TODO Show Filtered Quizzes based on Subject -->
       <v-subheader>Quiz</v-subheader>
       <v-list-item v-for="quiz in quizzes" :key="quiz.id">
-        {{quiz.title}}
+        {{ quiz.title }}
       </v-list-item>
->>>>>>> 62da190f6c0d425323858da46e62b5d62ec1ea4b
     </v-card>
   </div>
 </template>
 
 <script>
-import QuizListItem from './QuizListItem'
+import QuizListItem from "./QuizListItem";
 export default {
   name: "QuizList",
-  compnents: {QuizListItem},
+  compnents: { QuizListItem },
   data: () => ({
     show: false,
     selectedSubjects: [],
