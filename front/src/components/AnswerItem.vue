7aa0f00737006dad947d0ac76bca54e5cc882aed
davisa94 <austin.i.davis94@gmail.com>
2020-10-08 12:30:37 -0600

    Created asnswer Item ver. 1 with default answer object

---
+++ front/src/components/AnswerItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-card-title v-text="answer.body"/>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AnswerItem',
+  props: {
+  	answer: {
+      type: Object,
+      default:{id:0, body:"This is a test answer and should not appear"},
+  	},
+  }
+}
+</script>
