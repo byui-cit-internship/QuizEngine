c4214abdd41d7fee4aae103f2a154613d7956c35
davisa94 <austin.i.davis94@gmail.com>
2020-10-08 13:40:39 -0600

    Added question default

---
+++ front/src/components/QuestionItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <AnswerItem>
+    </AnswerItem>
+</template>
+
+<script>
+import AnswerItem from "./AnswerItem";
+export default {
+    name: 'QuestionItem',
+  props: {
+    question: {
+      type: Object,
+      default() {
+        return {
+            "id":0,
+            "body":"This is a test Question and should not appear", 
+            "answers":[
+            {"id":0, "body":"q Test Answer 1"},
+            {"id":1, "body":"q Test Answer 2"},
+            {"id":2, "body":"q Test Answer 3"},
+            ],
+            "correctAnswers":[1],
+            },
+           },
+}}}
+</script>
