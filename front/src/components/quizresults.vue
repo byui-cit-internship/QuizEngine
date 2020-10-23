e08fd7b544a140993b5c71e9273186933bf1955c
davisa94 <austin.i.davis94@gmail.com>
2020-10-23 16:18:56 -0600

    prepared the quizresults to accept an id

--- front/src/components/quizresults.vue
+++ front/src/components/quizresults.vue
@@ -1,11 +1,13 @@
 <template>
     <v-container>
-        
+        <FullQuiz :quizId="quizId"></FullQuiz>
     </v-container>
 </template>
 
 <script>
+import FullQuiz from "./FullQuiz";
 export default {
+    components: {FullQuiz},
     name:"QuizResults",
     data:() => ({
         //routing
@@ -14,6 +16,12 @@ export default {
         btn1: "Quiz List",
         btn2: "Create a Quiz",
         btn3: "Results"
-    })
+    }),
+    props: {
+        quizId: {
+            type: Number,
+            required: true
+        }
+    }
 };
 </script>
